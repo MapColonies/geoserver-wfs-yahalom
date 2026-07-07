6513970f3205ac4c3ca89f08d4bf8f50c5e92d33
Asaf Masa <asafmas@users.noreply.github.com>
2026-07-08 00:22:54 +0300

    feat: update to send request to geoserver

--- src/index.mjs
+++ src/index.mjs
@@ -20,7 +20,7 @@ const GEOSERVER_LOCAL_PORT = '8080'; // Default port for local GeoServer instanc
 const POLLING_INTERVAL_MS = env.get('POLLING_INTERVAL_MS').default(3000).asIntPositive(); // Polling interval in milliseconds
 const GEOSERVER_BASE_URL = env
   .get('GEOSERVER_BASE_URL')
-  .default('https://geoserver-manual-route-3d-dev.apps.j1lk3njp.eastus.aroapp.io/geoserver')
+  .default('https://localhost/geoserver')
   .asString();
 const GEOSERVER_LOCAL_BASE_URL = `http://localhost:${GEOSERVER_LOCAL_PORT}/geoserver`;
 
